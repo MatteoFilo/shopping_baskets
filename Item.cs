c0150f68ee48c1a1d8795ef887649feae628698d
MatteoFilo <matteo.filopanti@gmail.com>
2019-11-19 14:50:51 +0100

    Second Commit
    Program Commit

---
+++ Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace shopping_baskets
+{
+    public class Item
+    {
+        int qty;
+        string name;
+        bool taxable;
+        bool imported;
+        float price;
+
+        public double Calcola()
+        {
+            double ret;
+            if (taxable)
+            {
+                if (imported)
+                {
+
+                    ret = price * 1.15;
+                }
+                else
+                {
+                    ret = price * 1.10;
+                }
+            }
+            else
+            {
+                ret = price * 1.05;
+            }
+            return ret;   
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace shopping_baskets
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string row;
+            ArrayList valore = new ArrayList();
+            Console.WriteLine("Insert your shopping basket:");
+            do
+            {
+                row = Console.ReadLine();
+                Item itemRow = ReadString(row);
+                valore.Add(itemRow); 
+            } while (!String.IsNullOrWhiteSpace(row));
+
+            foreach (Item val in valore)
+                {
+                Console.WriteLine(val);
+            }
+            
+
+        }
+
+        private static Item ReadString(string row)
+        {
+
+            Item RowItem = new Item();
+
+            String[] elements = System.Text.RegularExpressions.Regex.Split(row, " ");
+            foreach (var element in elements)
+                if(element)
+
+            return RowItem;
+        }
+    }
+}
+
